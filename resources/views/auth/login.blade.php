1fc350eb67d1444ec4c179afb19943b52d26693a
edgkid <edglandaeta.15@gmail.com>
2019-11-29 14:40:25 -0400

    login con correo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,24 @@
 @extends('layouts.loginTemplate')
 
 @section('logincontent')
-<form class="m-t" role="form" action="{{ route('login') }}">
-	{{ csrf_field() }}
-    <div class="form-group {{ $errors->has('email') ? ' has-error' : '' }}">
-        <input type="email" class="form-control" placeholder="Username" required="">
-        @if ($errors->has('email'))
-            <span class="help-block"><strong>{{ $errors->first('email') }}</strong></span>
-        @endif
-    </div>
-    <div class="form-group {{ $errors->has('password') ? ' has-error' : '' }}">
-        <input type="password" class="form-control" placeholder="Password" required="">
-        @if ($errors->has('password'))
-            <span class="help-block"> <strong>{{ $errors->first('password') }}</strong> </span>
-        @endif
-    </div>
-    <button type="submit" class="btn btn-success block full-width m-b">Aceptar</button>
 
-    <a href="#"><small>¿Olvidaste tu Contraseña?</small></a>
+<form class="m-t" role="form" method="POST" action="{{ route('login') }}">
+  {{ csrf_field() }}
+  <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="email" required autofocus>
+    @if ($errors->has('email'))
+      <span class="help-block"><strong>{{ $errors->first('email') }}</strong></span>
+    @endif
+  </div>
+  <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+    <input id="password" type="password" class="form-control" name="password" placeholder="password" required>
+    @if ($errors->has('password'))
+      <span class="help-block"> <strong>{{ $errors->first('password') }}</strong> </span>
+    @endif
+  </div>
+      <button type="submit" class="btn btn-primary block full-width m-b">Aceptar </button>
+      <!-- <a class="btn btn-link" href="{{ route('password.request') }}">¿Olvidast tu contraseña?</a>-->
+      <a href="#"><small>¿Olvidaste tu Contraseña?</small></a>
 </form>
 
 @endsection
