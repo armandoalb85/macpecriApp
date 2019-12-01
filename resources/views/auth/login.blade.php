249854217e89262bc87c307e1e01bb843a617f69
edgkid <edglandaeta.15@gmail.com>
2019-11-30 22:41:18 -0400

    usename por email
    Se modifica login blade y controller login para iniciar sesion con username y no con email

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,14 +4,14 @@
 
 <form class="m-t" role="form" method="POST" action="{{ route('login') }}">
   {{ csrf_field() }}
-  <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-    <input id="email"  class="form-control" name="email" value="{{ old('email') }}" placeholder="email" autofocus>
-    @if ($errors->has('email'))
-      <span class="help-block"><strong>{{ $errors->first('email') }}</strong></span>
+  <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
+    <input id="email"  type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="usuario" autofocus>
+    @if ($errors->has('username'))
+      <span class="help-block"><strong>{{ $errors->first('username') }}</strong></span>
     @endif
   </div>
   <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-    <input id="password" type="password" class="form-control" name="password" placeholder="password">
+    <input id="password" type="password" class="form-control" name="password" placeholder="contraseña">
     @if ($errors->has('password'))
       <span class="help-block"> <strong>{{ $errors->first('password') }}</strong> </span>
     @endif
