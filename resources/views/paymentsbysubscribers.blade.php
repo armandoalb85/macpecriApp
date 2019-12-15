9e78e055fc7a3a3b0c740cb42996cc29479301ff
edgkid <edglandaeta.15@gmail.com>
2019-12-15 19:22:04 -0400

    esqueleto de la vista implementado

--- resources/views/paymentsbysubscribers.blade.php
+++ resources/views/paymentsbysubscribers.blade.php
@@ -1,4 +1,116 @@
 @extends('layout/template')
 @section('contentapp')
-<h1>Vista para Consulta de Pagos realizados</h1>
+<!-- Guia -->
+  <div class="row wrapper border-bottom white-bg page-heading">
+      <div class="col-lg-10">
+          <h2>Consulta de Pagos Realizados por los Suscriptores</h2>
+          <ol class="breadcrumb">
+              <li class="breadcrumb-item">
+                  <a href="index-2.html">Sistema Administrativo</a>
+              </li>
+              <li class="breadcrumb-item">
+                  <a>Pagos realizados</a>
+              </li>
+              <li class="breadcrumb-item active">
+                  <strong>Pagos realizados</strong>
+              </li>
+          </ol>
+      </div>
+  </div>
+<!-- Guia -->
+<div class="wrapper wrapper-content animated fadeInRight">
+  <div class="row">
+    <div class="col-lg-4">
+      <div class="ibox ">
+        <div class="ibox-title">
+          <h5>Consultar Pagos</h5>
+        </div>
+        <div class="ibox-content">
+          <form method="post" action="{{ url('boletines/nuevo') }}" >
+            {{csrf_field()}}
+            <br>
+            <div class="form-group row">
+              <label class="col-lg-3 col-form-label">Suscripción</label>
+            <div class="col-sm-9">
+              <select class="form-control m-b" name="status">
+                <option>Activo</option>
+                <option>Inactivo</option>
+              </select>
+            </div>
+            </div>
+
+
+            <div class="form-group row">
+              <label class="col-lg-3 col-form-label">Desde</label>
+              <div class="form-group col-lg-9" id="newsletterCalendar">
+                <div class="input-group date">
+                  <span class="input-group-addon">
+                    <i class="fa fa-calendar"></i></span>
+                    <input type="text" class="form-control" name="startdate">
+                </div>
+              </div>
+            </div>
+
+            <div class="form-group row">
+              <label class="col-lg-3 col-form-label">Hasta</label>
+              <div class="form-group col-lg-9" id="newsletterCalendar">
+                <div class="input-group date">
+                  <span class="input-group-addon">
+                    <i class="fa fa-calendar"></i></span>
+                    <input type="text" class="form-control" name="closedate">
+                </div>
+              </div>
+            </div>
+
+            <div class="form-group row">
+              <div class="col-lg-4">
+                <br>
+                <button class="btn btn-sm btn-primary col-12" type="submit">Aceptar</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-8">
+      <div class="ibox ">
+          <div  class="ibox-title">
+              <h5>Listado de Pagos Realizados</h5>
+          </div>
+          <div class="ibox-content">
+
+            <div class="table-responsive">
+              <table class="table table-striped table-bordered table-hover dataTables-example" >
+                  <thead>
+                  <tr>
+                      <th>Suscriptor</th>
+                      <th>Fecha</th>
+                      <th>Pago</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                        <td>Trident</td>
+                        <td>Internet
+                            Explorer 4.0
+                        </td>
+                        <td>Win 95+</td>
+                    </tr>
+                    <tr >
+                        <td>Trident</td>
+                        <td>Internet
+                            Explorer 5.0
+                        </td>
+                        <td>Win 95+</td>
+                    </tr>
+                  </tfoot>
+              </table>
+            </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
