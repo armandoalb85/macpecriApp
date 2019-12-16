2522916242b6f87ff27429e365fe8f4b85a3c58f
edgkid <edglandaeta.15@gmail.com>
2019-12-16 16:35:30 -0400

    Modelo y Migracion IpRecords
    Se crea modelo y migración para manejar los historicos de accesos de los suscriptores.

---
+++ database/migrations/2019_12_16_203216_create_ip_records_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIpRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ip_records', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('ip');
+            $table->string('location');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ip_records');
+    }
+}
