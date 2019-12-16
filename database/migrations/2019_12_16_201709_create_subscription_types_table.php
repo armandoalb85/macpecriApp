c7c06c3c0ecd3573c4e62c2dd76a1667f3afdb78
edgkid <edglandaeta.15@gmail.com>
2019-12-16 16:29:02 -0400

    Modelo y migracion Typo de suscripcion
    Se crea modelo y migración para tipo de suscripciones

---
+++ database/migrations/2019_12_16_201709_create_subscription_types_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscriptionTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscription_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->float('cost');
+            $table->integer('limit');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscription_types');
+    }
+}
