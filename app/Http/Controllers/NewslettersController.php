982c441c4383359890704dd2a049cbc58396157f
edgkid <edglandaeta.15@gmail.com>
2019-12-19 14:26:36 -0400

    Controller for  Newsletters
    Se crea cuerpo de controlador para los boletines informativos.

---
+++ app/Http/Controllers/NewslettersController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NewslettersController extends Controller
+{
+
+    /*
+    This method show a newsletter dashboard
+    */
+    public function indexNewsletters(){
+
+    }
+
+    /*
+    This method show a newsletter in specific
+    */
+    public function showNewsletters(){
+
+    }
+
+    /*
+    This method show a page for create newsletter
+    */
+    public function newNewsletters(){
+
+    }
+
+    /*
+    This method allow create a newsletter
+    */
+    public function saveNewsletters(){
+
+    }
+
+    /*
+    This method show a page for edit newsletter in specific
+    */
+    public function editNewsletters(){
+
+    }
+
+    /*
+    This method allow update a newsletter
+    */
+    public function updateNewsletters(){
+
+    }
+
+    /*
+    This method allow destroy a newsletter
+    */
+    public function destroyNewsletters(){
+
+    }
+}
