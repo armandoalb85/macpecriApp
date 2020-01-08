89771257d18af6b81c3cf54d09f476d83d6e25bb
edgkid <edglandaeta.15@gmail.com>
2020-01-08 12:02:23 -0400

    ajsute de escript mantenimiento

--- public/scrpits/complemento_db.sql
+++ public/scrpits/complemento_db.sql
@@ -1,3 +1,4 @@
+-- Trigger para crear usuario wp en sistema
 CREATE OR REPLACE TRIGGER new_admin_user
 AFTER INSERT ON wp_users
 FOR EACH ROW
@@ -8,5 +9,31 @@ BEGIN
         (SELECT user_email FROM wp_users WHERE id = (SELECT max(id) FROM wp_users )),
         MD5('Macpecri123#'),
         (SELECT user_nicename FROM wp_users WHERE id = (SELECT max(id) FROM wp_users )));
+
+END;
+
+---- Procedimiento para migrar usuarios Wp
+CREATE OR REPLACE PROCEDURE migrate_admin_wp_users()
+BEGIN
+
+DECLARE countValue INT;
+DECLARE totalRegister INT;
+
+    SET countValue = 1;
+
+    SELECT cont(id) INTO totalRegister
+    FROM wp_users;
+
+    WHILE countValue <= totalRegister DO
+
+        INSERT INTO users (name, email, password, username) VALUES (
+        (SELECT user_login FROM wp_users WHERE id = countValue ),
+        (SELECT user_email FROM wp_users WHERE id = countValue ),
+        MD5('Macpecri123#'),
+        (SELECT user_nicename FROM wp_users WHERE id = countValue ));
+
+        SET countValue = countValue + 1;
+
+    END WHILE;
 
 END;
