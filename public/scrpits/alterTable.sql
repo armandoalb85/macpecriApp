9c3d349f785f3d7c930dc5c4d33839e18dfcd2c6
edgkid <edglandaeta.15@gmail.com>
2020-01-08 10:54:08 -0400

    script para mantenimiento

--- public/scrpits/alterTable.sql
+++ public/scrpits/alterTable.sql
@@ -1,7 +1,29 @@
-Delete from subscriber_subscription_type;
-
-
+DELETE FROM subscriber_subscription_type;
+DELETE FROM newsletter_subscriber;
+DELETE FROM payment_account_statements;
+DELETE FROM payment_method_records;
+DELETE FROM payment_methods;
+DELETE FROM ip_records;
+DELETE FROM subscriber_histories;
+DELETE FROM subscribers;
+DELETE FROM subscription_types;
+DELETE FROM users;
+DELETE FROM newsletters;
+DELETE FROM subscription_messages;
+DELETE FROM Subscribe_nows;
+commit;
 
 ALTER TABLE subscriber_subscription_type AUTO_INCREMENT = 1;
-
+ALTER TABLE users AUTO_INCREMENT = 1;
+ALTER TABLE subscribers AUTO_INCREMENT = 1;
+ALTER TABLE subscription_types AUTO_INCREMENT = 1;
+ALTER TABLE newsletters AUTO_INCREMENT = 1;
+ALTER TABLE newsletter_subscriber AUTO_INCREMENT = 1;
+ALTER TABLE payment_methods AUTO_INCREMENT = 1;
+ALTER TABLE payment_method_records AUTO_INCREMENT = 1;
+ALTER TABLE payment_account_statements AUTO_INCREMENT = 1;
+ALTER TABLE ip_records AUTO_INCREMENT = 1;
+ALTER TABLE subscriber_histories AUTO_INCREMENT = 1;
+ALTER TABLE subscription_messages AUTO_INCREMENT = 1;
+ALTER TABLE Subscribe_nows AUTO_INCREMENT = 1;
 commit;
