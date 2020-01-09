ca933a239a9d3b31317442d70da99ce792b46406
edgkid <edglandaeta.15@gmail.com>
2020-01-08 22:03:46 -0400

    Ajuste de Trigger

--- public/scrpits/complemento_db.sql
+++ public/scrpits/complemento_db.sql
@@ -3,12 +3,16 @@ CREATE OR REPLACE TRIGGER new_admin_user
 AFTER INSERT ON wp_users
 FOR EACH ROW
 BEGIN
+    DECLARE maxAdminUserId INT;
+
+    SELECT max(id) INTO maxAdminUserId
+    FROM wp_users;
 
     INSERT INTO users (name, email, password, username) VALUES (
-        (SELECT user_login FROM wp_users WHERE id = (SELECT max(id) FROM wp_users )),
-        (SELECT user_email FROM wp_users WHERE id = (SELECT max(id) FROM wp_users )),
+        (SELECT user_login FROM wp_users WHERE id = maxAdminUserId),
+        (SELECT user_email FROM wp_users WHERE id = maxAdminUserId),
         MD5('Macpecri123#'),
-        (SELECT user_nicename FROM wp_users WHERE id = (SELECT max(id) FROM wp_users )));
+        (SELECT user_nicename FROM wp_users WHERE id = maxAdminUserId ));
 
 END;
 
