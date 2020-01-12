cc1f86d3efcf4e021c1bba471870ea5a724a7a54
edgkid <edglandaeta.15@gmail.com>
2020-01-12 05:38:12 -0400

    traduccion de datat table

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -140,10 +140,31 @@
     <script>
         $(document).ready(function(){
             $('.dataTables-example').DataTable({
+                "language": {
+                  "decimal": "",
+                  "emptyTable": "No hay información",
+                  "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+                  "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+                  "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+                  "infoPostFix": "",
+                  "thousands": ",",
+                  "lengthMenu": "Mostrar _MENU_ Entradas",
+                  "loadingRecords": "Cargando...",
+                  "processing": "Procesando...",
+                  "search": "Buscar:",
+                  "zeroRecords": "Sin resultados encontrados",
+                  "paginate": {
+                        "first": "Primero",
+                        "last": "Ultimo",
+                        "next": "Siguiente",
+                        "previous": "Anterior"
+                    }
+                },
                 pageLength: 10,
                 responsive: true,
                 dom: '<"html5buttons"B>lTfgitp',
-                buttons: []});
+                buttons: []
+              });
         });
     </script>
     <script>
