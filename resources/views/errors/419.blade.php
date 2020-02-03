7e8d6bc2d8a5b9742e58b47ee8602da8cdd6ae74
edgkid <edglandaeta.15@gmail.com>
2020-02-02 23:20:40 -0400

    error 419

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts/errorsTemplate')
+@section('errorcontent')
+<div class="middle-box text-center animated fadeInDown">
+    <h1>419</h1>
+    <h3 class="font-bold">Acceso no Autorizado</h3>
+
+    <div class="error-desc">
+        Tu sesión ha expirado. Por favor, actualice y pruebe de nuevo
+        <a href="{{ url('dashboard') }}"><img src="{{ asset('/img/macpecri_dash.png') }}" style="width:100%; height:auto; margin-top: 30%;"></a>
+    </div>
+
+</div>
+
+@endsection
