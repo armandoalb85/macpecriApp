a1ae2daf6fb358921a4aa2d8fa405df2242ed781
edgkid <edglandaeta.15@gmail.com>
2020-02-09 16:34:46 -0400

    Ajuste de bug
    Cambio Bug

--- app/Http/Controllers/specialsController.php
+++ app/Http/Controllers/specialsController.php
@@ -14,6 +14,15 @@ class specialsController extends Controller
     $startDate = $startdate;
     $closeDate = $closedate;
 
+    //echo  $type.'<br>';
+    //echo $startdate.'<br>';
+    //echo $closedate.'<br>';
+
+    //echo 'set <br>';
+
+    //echo $startDate.'<br>';
+    //echo $closeDate.'<br>';
+
     if ($typeSubscribers != 'Todos'){
       $queryResults = DB::table('subscribers')
         ->join('subscriber_subscription_type', 'subscribers.id', '=', 'subscriber_subscription_type.subscriber_id')
@@ -23,7 +32,7 @@ class specialsController extends Controller
         ->where('subscriber_subscription_type.startdate','<=', $closedate)
         ->where('subscription_types.type', '=', $type)
         ->whereNull('subscriber_subscription_type.closedate')
-        ->select('subscribers.id','subscribers.name', 'subscribers.lastname', 'users.email', 'subscriber_subscription_type.status', 'subscription_types.type')
+        ->select('subscribers.id','subscribers.name', 'subscribers.lastname', 'users.email', 'subscriber_subscription_type.status', 'subscription_types.type', 'subscriber_subscription_type.startdate')
         ->get();
     }else{
       $queryResults = DB::table('subscribers')
@@ -33,9 +42,11 @@ class specialsController extends Controller
         ->where('subscriber_subscription_type.startdate','>=', $startdate)
         ->where('subscriber_subscription_type.startdate','<=', $closedate)
         ->whereNull('subscriber_subscription_type.closedate')
-        ->select('subscribers.id','subscribers.name', 'subscribers.lastname', 'users.email', 'subscriber_subscription_type.status', 'subscription_types.type')
+        ->select('subscribers.id','subscribers.name', 'subscribers.lastname', 'users.email', 'subscriber_subscription_type.status', 'subscription_types.type', 'subscriber_subscription_type.startdate')
         ->get();
     }
+
+    //echo $queryResults.'<br>';
 
     return view('subscribermanager',compact('queryResults', 'typeSubscribers', 'startDate', 'closeDate'));
   }
