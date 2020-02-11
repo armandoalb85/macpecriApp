8d222d43dfcfa364ca4861c918606b9a2968e64c
armandoalb85 <45047439+armandoalb85@users.noreply.github.com>
2020-02-10 23:55:42 -0400

    Ajuste de entrega en el servidor

--- public/scrpits/trigger.sql
+++ public/scrpits/trigger.sql
@@ -1,3 +1,4 @@
+----------------------------Version Local desarrollo
 -- Trigger para crear usuario wp en sistema
 CREATE OR REPLACE TRIGGER new_admin_user
 AFTER INSERT ON wp_users
@@ -17,28 +18,22 @@ BEGIN
 
 END;
 
---- Procedimiento para migrar usuarios Wp
-CREATE OR REPLACE PROCEDURE migrate_admin_wp_users()
+---Version de entrega
+CREATE TRIGGER new_admin_user
+AFTER INSERT ON wp_users
+FOR EACH ROW
+
 BEGIN
+    DECLARE maxAdminUserId INT;
 
-DECLARE countValue INT;
-DECLARE totalRegister INT;
-
-    SET countValue = 1;
-
-    SELECT cont(id) INTO totalRegister
+    SELECT max(`id`) INTO maxAdminUserId
     FROM wp_users;
 
-    WHILE countValue <= totalRegister DO
-
-        INSERT INTO users (name, email, password, username) VALUES (
-        (SELECT user_login FROM wp_users WHERE id = countValue ),
-        (SELECT user_email FROM wp_users WHERE id = countValue ),
+    INSERT INTO `users` (name, email, password, username, `type`) VALUES (
+        (SELECT user_login FROM wp_users WHERE `id` = maxAdminUserId),
+        (SELECT user_email FROM wp_users WHERE `id`= maxAdminUserId),
         MD5('Macpecri123#'),
-        (SELECT user_nicename FROM wp_users WHERE id = countValue ));
-
-        SET countValue = countValue + 1;
-
-    END WHILE;
+        (SELECT user_nicename FROM wp_users WHERE `id` = maxAdminUserId ),
+        'Admin');
 
 END;
