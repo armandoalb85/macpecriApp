7ed64ba6838099216795ec0b59e83906b15722b0
Armando <armandoalb85@gmail.com>
2020-03-17 16:23:30 -0400

    prueba de commit

--- app/PaymentMethod.php
+++ app/PaymentMethod.php
@@ -10,6 +10,6 @@ class PaymentMethod extends Model
     *This method define an asociation between PaymentMethod with PaymentMethodRecord
     */
     public function paymentMethodRecords(){
-      return $this->->hasMany('App\PaymentMethodRecord');
+      return $this->hasMany('App\PaymentMethodRecord');
     }
 }
